6098084403e434ef2480a6d5397a4b5a210dc1ad
pozopardo <pozopardo@tu-berlin.de>
2015-02-13 11:49:00 +0100

    Testing EGit

--- src/main/java/es/denis/thinking/capOne/ATypeName.java
+++ src/main/java/es/denis/thinking/capOne/ATypeName.java
@@ -7,5 +7,7 @@ public class ATypeName {
 		ATypeName aTypeName = new ATypeName();
 		System.out.println("This is exercice 3");
 		System.out.println("It just creates the object: " + aTypeName.toString());
+		System.out.println("The End");
+		
 	}
 }
