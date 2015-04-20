fd68c1f9dd43faf78fc008bf67372220d688924f
pozopardo <pozopardo@tu-berlin.de>
2015-04-21 01:00:19 +0200

    ADDED exercice #8 chapter #2

---
+++ src/main/java/es/denis/thinking/chapTwo/LongWithOctalAndHex.java
@@ -0,0 +1,17 @@
+package es.denis.thinking.chapTwo;
+
+import static es.denis.thinking.utils.Print.*;
+
+//Chapter - Operators: Exercise 3
+
+public class LongWithOctalAndHex {
+
+	public static void main(String[] args) {
+		long a = 0xaf;
+		long b = 0123;
+		
+		print("A(HEX): AF -> " + Long.toBinaryString(a));
+		print("B(OCT): 123 -> " + Long.toBinaryString(b));
+	}
+
+}
