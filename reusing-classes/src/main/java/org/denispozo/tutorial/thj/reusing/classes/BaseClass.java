2546c6f4fd9be63fa9600e2fa328706a37b3454e
Denis Pozo <denis.pozo@dai-labor.de>
2016-03-09 11:16:28 +0100

    add exercise 8 - completed

---
+++ reusing-classes/src/main/java/org/denispozo/tutorial/thj/reusing/classes/BaseClass.java
@@ -0,0 +1,17 @@
+package org.denispozo.tutorial.thj.reusing.classes;
+
+import static org.denispozo.tutorial.thj.util.Print.*; 
+
+/*
+ * Chapter - Reusing Classes
+ * Section - Initializing the base class
+ * Exercise 8 
+ */
+
+class BaseClass {
+	
+	BaseClass(int i) {
+		print("BaseClass constructor");
+	}
+
+}
---
+++ reusing-classes/src/main/java/org/denispozo/tutorial/thj/reusing/classes/DerivedClass.java
@@ -0,0 +1,22 @@
+package org.denispozo.tutorial.thj.reusing.classes;
+
+import static org.denispozo.tutorial.thj.util.Print.*; 
+
+/*
+ * Chapter - Reusing Classes
+ * Section - Initializing the base class
+ * Exercise 8 
+ */
+
+public class DerivedClass extends BaseClass {
+
+	public DerivedClass() {
+		super(0);
+		print("DerivedClass default constructor");
+	}
+	
+	public DerivedClass(int i) {
+		super(i);
+		print("DerivedClass non-default constructor");		
+	}
+}
