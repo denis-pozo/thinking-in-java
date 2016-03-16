8a11cf7cc7b1e4651bac4cc823af0907e57bdf00
Denis Pozo <denis.pozo@dai-labor.de>
2016-03-16 10:36:26 +0100

    add completed exercises 10 of poly

---
+++ polymorphism/src/main/java/org/denispozo/tutorial/thj/polymorphism/undefined/BaseClass.java
@@ -0,0 +1,21 @@
+package org.denispozo.tutorial.thj.polymorphism.undefined;
+
+import static org.denispozo.tutorial.thj.util.Print.*; 
+
+/*
+ * Chapter - Polymorphism
+ * Section - Extensibility
+ * Exercise 10
+ */
+
+public class BaseClass {
+
+	public void method1 () {
+		print("BaseClass.method1()");
+		method2();
+	}
+	
+	public void method2 () {
+		print("BaseClass.method2()");
+	}
+}
---
+++ polymorphism/src/main/java/org/denispozo/tutorial/thj/polymorphism/undefined/SubClass.java
@@ -0,0 +1,24 @@
+package org.denispozo.tutorial.thj.polymorphism.undefined;
+
+import static org.denispozo.tutorial.thj.util.Print.*; 
+
+/*
+ * Chapter - Polymorphism
+ * Section - Extensibility
+ * Exercise 10
+ */
+
+public class SubClass extends BaseClass{
+
+	@Override
+	public void method2() {
+		print("Subclass.method2()");
+	}
+	
+	public static void main(String [] args) {
+		BaseClass object = new SubClass();
+		// If object calls method1: first method1 from baseClass
+		// will be called and then method2 of the subclass
+		object.method1();
+	}
+}
