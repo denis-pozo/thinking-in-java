ef922f3957f1399e6e51d332d2ae0647bd2fb330
Denis Pozo <denis.pozo@dai-labor.de>
2016-03-29 21:21:53 +0200

    add exercise 13 of chapter interfaces - finished

---
+++ interfaces/src/main/java/org/denispozo/tutorial/thj/interfaces/multiplying/SubSubinterface.java
@@ -0,0 +1,11 @@
+package org.denispozo.tutorial.thj.interfaces.multiplying;
+
+/*
+ * Chapter - Interfaces
+ * Section - Multiple inheritance in Java
+ * Exercise 13
+ */
+
+public interface SubSubinterface extends SubinterfaceA, SubinterfaceB {
+
+}
