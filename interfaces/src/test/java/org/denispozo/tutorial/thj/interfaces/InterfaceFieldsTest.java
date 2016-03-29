aace8157f803183eb81c6c183d437b816dbb4610
Denis Pozo <denis.pozo@dai-labor.de>
2016-03-29 23:33:30 +0200

    add exercise 17 of chapter interfaces - finished

---
+++ interfaces/src/test/java/org/denispozo/tutorial/thj/interfaces/InterfaceFieldsTest.java
@@ -0,0 +1,22 @@
+package org.denispozo.tutorial.thj.interfaces;
+
+import static org.denispozo.tutorial.thj.util.Print.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*
+ * Chapter - Interfaces
+ * Section - Fields in interfaces
+ * Exercise 17
+ */
+
+public class InterfaceFieldsTest {
+	
+	@Test
+	public void proveThatTheFieldsAreStaticAndFinal() {
+		print("Test of exercise 17 - proveThatTheFieldsAreStaticAndFinal");
+		
+		assertEquals(10, InterfaceWithFields.NUMBER);
+	}
+}
