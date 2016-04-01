d4a5122d33803b5f0b75ab8c24596c875a134098
Denis Pozo <denis.pozo@dai-labor.de>
2016-04-01 10:23:45 +0200

    add Inner Classes chapter

---
+++ inner-classes/src/main/java/org/denispozo/tutorial/thj/innerclasses/App.java
@@ -0,0 +1,13 @@
+package org.denispozo.tutorial.thj.innerclasses;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
