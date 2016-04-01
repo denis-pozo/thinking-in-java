6d59d852e0c99a6d8a57064224b9556f22763989
Denis Pozo <denis.pozo@dai-labor.de>
2016-04-01 10:37:53 +0200

    add exercise 1 of inner classes

---
+++ inner-classes/src/main/java/org/denispozo/tutorial/thj/innerclasses/inandout/Outer.java
@@ -0,0 +1,33 @@
+package org.denispozo.tutorial.thj.innerclasses.inandout;
+
+import static org.denispozo.tutorial.thj.util.Print.*;
+
+/*
+ * Chapter - Inner Classes
+ * Section - Creating inner classes
+ * Exercise 1
+ */
+
+public class Outer {
+	
+	public Outer() {
+		print("Outer()");
+	}
+	
+	public Inner getInner() {
+		return new Inner();
+	}
+	
+	class Inner {
+		public Inner() {
+			print("Outer.Inner()");
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	public static void main(String [] args) {
+		Outer outer = new Outer();
+		Outer.Inner inner = outer.getInner();		
+	}
+
+}
