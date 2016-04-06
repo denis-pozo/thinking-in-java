33bc9080b608183c061220a7e69296bdf7fff7fd
Denis Pozo <denis.pozo@dai-labor.de>
2016-04-06 11:26:11 +0200

    add exercise 12 - inner classes chapter

---
+++ inner-classes/src/main/java/org/denispozo/tutorial/thj/innerclasses/anonymous/Outer.java
@@ -0,0 +1,43 @@
+package org.denispozo.tutorial.thj.innerclasses.anonymous;
+
+import static org.denispozo.tutorial.thj.util.Print.*;
+
+/*
+ * Chapter - Inner Classes
+ * Section - Anonymous inner classes
+ * Exercise 12
+ */
+
+public class Outer {
+	
+	private int oi = 1;
+	
+	private void hi() {
+		print("Outer hi");
+	}
+	
+	public Inner inner() {
+		return new Inner() {
+			public void modifyOuter() {
+				oi *= 2;
+				hi();
+			}
+		};
+	}
+	
+	public void showOi(){
+		print(oi);
+	}
+	
+	public static void main(String [] args) {
+		Outer outer = new Outer();
+		outer.showOi();
+		outer.inner().modifyOuter();
+		outer.showOi();
+	}
+}
+
+
+interface Inner {
+	void modifyOuter();
+}
