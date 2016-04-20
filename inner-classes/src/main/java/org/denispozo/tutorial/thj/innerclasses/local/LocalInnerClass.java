e877a1937e4535065a746c33eeb8cfbdeb3b92e6
Denis Pozo <denis.pozo@dai-labor.de>
2016-04-20 15:21:01 +0200

    add example local inner of inner classes

---
+++ inner-classes/src/main/java/org/denispozo/tutorial/thj/innerclasses/local/LocalInnerClass.java
@@ -0,0 +1,65 @@
+package org.denispozo.tutorial.thj.innerclasses.local;
+
+import static org.denispozo.tutorial.thj.util.Print.*;
+
+/*
+ * Chapter - Inner Classes
+ * Section - Local Inner Classes
+ * Example
+ */
+
+public class LocalInnerClass {
+	
+	private int count = 0;
+	
+	Counter getCounter(final String name) {
+		// A local inner class 
+		class LocalCounter implements Counter {
+			public LocalCounter() {
+				print("LocalCounter()");
+			}
+
+			@Override
+			public int next() {
+				print(name);
+				return count++;
+			}
+		}
+		return new LocalCounter();
+	}
+	
+	// The same thing with an anonymous inner class:
+	Counter getCounter2(final String name) {
+		return new Counter() {
+			// Anonymous inner class cannot have a named
+			// constructor, only an instance initializer:
+			{
+				print("AnonymousCounter()");
+			}
+						
+			public int next() {
+				print(name);
+				return count++;
+			}
+		};
+	}
+	
+	public static void main (String [] args) {
+		LocalInnerClass lic = new LocalInnerClass();
+		Counter 
+			c1 = lic.getCounter("Local inner"),
+			c2 = lic.getCounter2("Anonymous inner");
+		
+		for(int i=0 ; i < 5 ; i++){
+			print(c1.next());
+		}
+		
+		for(int i=0 ; i < 5 ; i++){
+			print(c2.next());
+		}
+	}
+}
+
+interface Counter {
+	int next();
+}
