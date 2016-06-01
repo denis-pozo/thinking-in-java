df65c4252d2731a145edff62a2a03a0dfbd91a2b
Denis Pozo <denis.pozo@dai-labor.de>
2016-06-01 17:17:07 +0200

    create typeinfo project, necessary for some exercises necessary for holding objects and add dependencies

---
+++ holding-objects/src/main/java/org/denispozo/tutorial/thj/holding/objects/list/ListFeatures.java
@@ -0,0 +1,15 @@
+package org.denispozo.tutorial.thj.holding.objects.list;
+
+import org.denispozo.tutorial.thj.typeinfo.pets.*;
+
+import java.util.List;
+import java.util.Random;
+
+public class ListFeatures {
+	
+	public static void main(String[] args) {
+		Random rand = new Random(47);
+		List<Pet> pets = Pets.arrayList(7);
+	}
+
+}
---
+++ type-info/src/main/java/org/denispozo/tutorial/thj/typeinfo/pets/Cat.java
@@ -0,0 +1,6 @@
+package org.denispozo.tutorial.thj.typeinfo.pets;
+
+public class Cat extends Pet {
+  public Cat(String name) { super(name); }
+  public Cat() { super(); }
+} ///:~
