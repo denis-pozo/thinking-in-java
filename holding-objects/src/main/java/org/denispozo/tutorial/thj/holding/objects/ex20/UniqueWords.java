ad33610ce11c280af3880279e384274b683d6ffa
Denis Pozo <denis.pozo@dai-labor.de>
2016-07-12 07:59:23 +0200

    add exercise 20 of holding your objects

---
+++ holding-objects/src/main/java/org/denispozo/tutorial/thj/holding/objects/ex20/UniqueWords.java
@@ -0,0 +1,43 @@
+package org.denispozo.tutorial.thj.holding.objects.ex20;
+
+/*
+ * Chapter - Holding your objects
+ * Section - Set
+ * Exercise 16
+ */
+import static org.denispozo.tutorial.thj.util.PrintUtil.print;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.denispozo.tutorial.thj.util.TextFile;
+
+public class UniqueWords {
+	
+	public static void main(String[] args) {
+		int count = 0;
+		Map<Character, Integer> vowels = new HashMap<Character, Integer>();
+		Set<String> words = new TreeSet<String>(new TextFile("quixote.txt", "\\W+"));
+		
+		for(String word : words){
+			print(word + " has " + word.length() + " characters");
+			for(Character ch : word.toCharArray()){
+				count ++;
+				int vowelCounter = 0;
+				
+				if(vowels.containsKey(ch)){
+					vowelCounter = vowels.get(ch);
+				}
+				vowels.put(ch, vowelCounter+1);
+			}
+			
+		}
+		print("Characters inserted: " + count);
+		print("Characters stored: " + vowels.size());
+		print(vowels);
+	}
+
+	 
+}
