083efbfd68aa0befd5e642cd76436a63af01f600
Denis Pozo <denis.pozo@dai-labor.de>
2016-07-13 09:40:54 +0200

    add exercise 27 of holding your objets

---
+++ holding-objects/src/main/java/org/denispozo/tutorial/thj/holding/objects/queue/CommandQueuerEx27.java
@@ -0,0 +1,26 @@
+package org.denispozo.tutorial.thj.holding.objects.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * Chapter - Holding your objects
+ * Section - Queue
+ * Exercise 27
+ */
+
+public class CommandQueuerEx27 {
+	
+	@SuppressWarnings("rawtypes")
+	public static Queue fulfill(){
+		Queue<CommandEx27> queue = new LinkedList<CommandEx27>();
+		
+		queue.offer(new CommandEx27("jump"));
+		queue.offer(new CommandEx27("run"));
+		queue.offer(new CommandEx27("climb"));
+		queue.offer(new CommandEx27("drive"));
+		queue.offer(new CommandEx27("walk"));
+
+		return queue;
+	}
+}
---
+++ holding-objects/src/main/java/org/denispozo/tutorial/thj/holding/objects/queue/QueueConsumerEx27.java
@@ -0,0 +1,21 @@
+package org.denispozo.tutorial.thj.holding.objects.queue;
+
+import java.util.Queue;
+
+/*
+ * Chapter - Holding your objects
+ * Section - Queue
+ * Exercise 27
+ */
+
+public class QueueConsumerEx27 {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unchecked")
+		Queue<CommandEx27> queue = CommandQueuerEx27.fulfill();
+		
+		while(queue.peek() != null){
+			queue.poll().operation();
+		}
+	}
+}
