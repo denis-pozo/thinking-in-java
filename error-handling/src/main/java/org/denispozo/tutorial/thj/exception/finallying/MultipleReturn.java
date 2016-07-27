050e82018f30b0ecb462b801b4571a1a5d7cb137
Denis Pozo <denis.pozo@dai-labor.de>
2016-07-27 12:01:30 +0200

    add exercise16 of section Performing cleanup with finally of error handling

---
+++ error-handling/src/main/java/org/denispozo/tutorial/thj/exception/finallying/MultipleReturn.java
@@ -0,0 +1,35 @@
+package org.denispozo.tutorial.thj.exception.finallying;
+
+import static org.denispozo.tutorial.thj.util.PrintUtil.print;
+
+/*
+ * Chapter - Error Handling with Exceptions
+ * Section - Using finally during return
+ * Example
+ */
+
+public class MultipleReturn {
+	
+	public static void f(int i){
+		print("Initialization that requires cleanup");
+		try{
+			print("Point 1");
+			if(i == 1) return;
+			print("Point 2");
+			if(i == 2) return;
+			print("Point 3");
+			if(i == 3) return;
+			print("End");
+			return;		
+		} finally {
+			print("Performing cleanup");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i<=4 ; i++){
+			f(i);
+		}
+	}
+}
