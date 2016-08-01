58dbbadd05a5eb14510f1e54eb36464ed8c7ac79
Denis Pozo <denis.pozo@dai-labor.de>
2016-08-01 11:36:30 +0200

    add example InputFile of chapter error handling

---
+++ error-handling/src/main/java/org/denispozo/tutorial/thj/exception/constructor/InputFile.java
@@ -0,0 +1,60 @@
+package org.denispozo.tutorial.thj.exception.constructor;
+
+import static org.denispozo.tutorial.thj.util.PrintUtil.print;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ * Chapter - Error Handling with Exceptions
+ * Section - Exception Restrictions
+ * Example
+ */
+
+public class InputFile {
+
+	private BufferedReader in;
+	
+	public InputFile(String fileName) throws Exception {
+		try{
+			in = new BufferedReader(new FileReader(fileName));
+			// Other code that might throw exceptions
+		} catch (FileNotFoundException e) {
+			print("Could not open " + fileName);
+			throw e;
+		} catch(Exception e) {
+			// All other exceptions must close it
+			try{
+				in.close();
+			} catch(IOException ioe) {
+				print("in.close() unsuccessful");
+			}
+			throw e;
+		} finally {
+			// Don't close it here !!!
+		}
+	}
+	
+	public String getLine() {
+		String s;
+		
+		try {
+			s = in.readLine();
+		} catch(IOException e) {
+			throw new RuntimeException("readLine() failed");
+		}
+		
+		return s;
+	}
+	
+	public void dispose() {
+		try {
+			in.close();
+			print("dispose() successful");
+		} catch (IOException e2) {
+			throw new RuntimeException("in.close() failed");
+		}
+	}
+}
