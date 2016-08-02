25552d5c730e8e481289881ac4bf0cba26a3ac5f
Denis Pozo <denis.pozo@dai-labor.de>
2016-08-02 12:06:51 +0200

    add exercise 30 of chapter error handling, section converting checked to unchecked exceptions

---
+++ error-handling/src/main/java/org/denispozo/tutorial/thj/exception/converter/HumanEx30.java
@@ -0,0 +1,70 @@
+package org.denispozo.tutorial.thj.exception.converter;
+
+import static org.denispozo.tutorial.thj.util.PrintUtil.print;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/*
+ * Chapter - Error Handling with Exceptions
+ * Section - Converting checked to unchecked exceptions
+ * Exercise 30
+ */
+
+public class HumanEx30 {
+	
+	public static void main(String[] args) {
+		WrapCheckedExceptionEx30 wce = new WrapCheckedExceptionEx30();
+		
+		//You can call throwRuntimeException() without a try
+		//block and let RE leave the method: 
+		wce.throwRuntimeException(3);
+		//Or you can choose to catch exceptions:
+		for(int i = 0 ; i < 4 ; i++){
+			try {
+				if(i < 3){
+					wce.throwRuntimeException(i);
+				} else {
+					throw new RuntimeException();
+				}
+			} catch(RuntimeException re) {
+				try {
+					throw re.getCause();
+				} catch(Annoyance a) {
+					print("Annoyance: " + a);
+				} catch(Sneeze s) {
+					print("InputOutputException: " + s);
+				} catch (Throwable e) { 
+					print("Throwable: " + e);
+				}
+			}
+		}
+	}
+}
+
+class WrapCheckedExceptionEx30 {
+	void throwRuntimeException(int type) {
+		try {
+			switch (type) {
+			case 0:
+				throw new Annoyance();
+			case 1:
+				throw new Sneeze();
+			case 2:
+				throw new RuntimeException("Where am I?");
+			default:
+				return;
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
+
+class Annoyance extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
+
+class Sneeze extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
