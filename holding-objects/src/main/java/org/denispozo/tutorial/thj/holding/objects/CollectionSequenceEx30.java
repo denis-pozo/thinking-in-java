3f7f167c8f8aa3025c9a518c3535258726893b2f
Denis Pozo <denis.pozo@dai-labor.de>
2016-08-19 16:37:03 +0200

    comment all classes of holding objects utils proper classes are implemented

--- holding-objects/src/main/java/org/denispozo/tutorial/thj/holding/objects/CollectionSequenceEx30.java
+++ holding-objects/src/main/java/org/denispozo/tutorial/thj/holding/objects/CollectionSequenceEx30.java
@@ -1,113 +1,113 @@
-package org.denispozo.tutorial.thj.holding.objects;
-
-import java.util.Collection;
-import java.util.Iterator;
-
-import org.denispozo.tutorial.thj.typeinfo.pets.Pet;
-import org.denispozo.tutorial.thj.typeinfo.pets.Pets;
-
-/*
- * Chapter - Holding your objects
- * Section - InterfaceVsIterator
- * Exercise 30
- */
-
-public class CollectionSequenceEx30 implements Collection<Pet> {
-
-	private Pet[] pets = Pets.createArray(8);
-	
-	@Override
-	public Iterator<Pet> iterator() {
-		return new Iterator<Pet>() {
-			private int index = 0;
-			
-			public boolean hasNext() {
-				return index < pets.length;
-			}
-			
-			public Pet next() {
-				return pets[index++];
-			}
-			
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
-		};
-	}
-	
-	@Override
-	public int size() {
-		return pets.length;
-	}
-	
-	@Override
-	public boolean isEmpty() {
-		return (this.size() == 0) ? true : false;
-	}
-
-	@Override
-	public boolean contains(Object o) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Object[] toArray() {
-		return pets;
-	}
-
-	@Override
-	public <T> T[] toArray(T[] a) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean add(Pet e) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends Pet> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@SuppressWarnings("unused")
-	@Override
-	public void clear() {
-		
-		if(this.size() != 0){
-			for(Pet p : pets){
-				p = null;
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		CollectionSequenceEx30 c = new CollectionSequenceEx30();
-		
-		InterfaceVsIterator.display(c);
-		InterfaceVsIterator.display(c.iterator());
-	
-	}
-	
-	
-}
+//package org.denispozo.tutorial.thj.holding.objects;
+//
+//import java.util.Collection;
+//import java.util.Iterator;
+//
+//import org.denispozo.tutorial.thj.typeinfo.pets.Pet;
+//import org.denispozo.tutorial.thj.typeinfo.pets.Pets;
+//
+///*
+// * Chapter - Holding your objects
+// * Section - InterfaceVsIterator
+// * Exercise 30
+// */
+//
+//public class CollectionSequenceEx30 implements Collection<Pet> {
+//
+//	private Pet[] pets = Pets.createArray(8);
+//	
+//	@Override
+//	public Iterator<Pet> iterator() {
+//		return new Iterator<Pet>() {
+//			private int index = 0;
+//			
+//			public boolean hasNext() {
+//				return index < pets.length;
+//			}
+//			
+//			public Pet next() {
+//				return pets[index++];
+//			}
+//			
+//			public void remove() {
+//				throw new UnsupportedOperationException();
+//			}
+//		};
+//	}
+//	
+//	@Override
+//	public int size() {
+//		return pets.length;
+//	}
+//	
+//	@Override
+//	public boolean isEmpty() {
+//		return (this.size() == 0) ? true : false;
+//	}
+//
+//	@Override
+//	public boolean contains(Object o) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	@Override
+//	public Object[] toArray() {
+//		return pets;
+//	}
+//
+//	@Override
+//	public <T> T[] toArray(T[] a) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	@Override
+//	public boolean add(Pet e) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	@Override
+//	public boolean remove(Object o) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	@Override
+//	public boolean containsAll(Collection<?> c) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	@Override
+//	public boolean addAll(Collection<? extends Pet> c) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	@Override
+//	public boolean removeAll(Collection<?> c) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	@Override
+//	public boolean retainAll(Collection<?> c) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	@SuppressWarnings("unused")
+//	@Override
+//	public void clear() {
+//		
+//		if(this.size() != 0){
+//			for(Pet p : pets){
+//				p = null;
+//			}
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//		CollectionSequenceEx30 c = new CollectionSequenceEx30();
+//		
+//		InterfaceVsIterator.display(c);
+//		InterfaceVsIterator.display(c.iterator());
+//	
+//	}
+//	
+//	
+//}
--- holding-objects/src/main/java/org/denispozo/tutorial/thj/holding/objects/NonCollectionSequence.java
+++ holding-objects/src/main/java/org/denispozo/tutorial/thj/holding/objects/NonCollectionSequence.java
@@ -1,44 +1,44 @@
-package org.denispozo.tutorial.thj.holding.objects;
-
-import java.util.Iterator;
-
-import org.denispozo.tutorial.thj.typeinfo.pets.Pet;
-import org.denispozo.tutorial.thj.typeinfo.pets.Pets;
-
-/*
- * Chapter - Holding your objects
- * Section - InterfaceVsIterator
- * Book Example
- */
-
-public class NonCollectionSequence extends PetSequence {
-
-	public Iterator<Pet> iterator() {
-		return new Iterator<Pet>() {
-			private int index = 0;
-			
-			public boolean hasNext() {
-				return index < pets.length;
-			}
-			
-			public Pet next() {
-				return pets[index++];
-			}
-			
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
-		};
-	}
-	
-	public static void main(String[] args) {
-		NonCollectionSequence c = new NonCollectionSequence();
-		InterfaceVsIterator.display(c.iterator());
-	
-	}
-}
-
-class PetSequence {
-	
-	protected Pet[] pets = Pets.createArray(8); 
-}
+//package org.denispozo.tutorial.thj.holding.objects;
+//
+//import java.util.Iterator;
+//
+//import org.denispozo.tutorial.thj.typeinfo.pets.Pet;
+//import org.denispozo.tutorial.thj.typeinfo.pets.Pets;
+//
+///*
+// * Chapter - Holding your objects
+// * Section - InterfaceVsIterator
+// * Book Example
+// */
+//
+//public class NonCollectionSequence extends PetSequence {
+//
+//	public Iterator<Pet> iterator() {
+//		return new Iterator<Pet>() {
+//			private int index = 0;
+//			
+//			public boolean hasNext() {
+//				return index < pets.length;
+//			}
+//			
+//			public Pet next() {
+//				return pets[index++];
+//			}
+//			
+//			public void remove() {
+//				throw new UnsupportedOperationException();
+//			}
+//		};
+//	}
+//	
+//	public static void main(String[] args) {
+//		NonCollectionSequence c = new NonCollectionSequence();
+//		InterfaceVsIterator.display(c.iterator());
+//	
+//	}
+//}
+//
+//class PetSequence {
+//	
+//	protected Pet[] pets = Pets.createArray(8); 
+//}
