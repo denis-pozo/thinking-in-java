9b2c9b034c0bd5df44dc0b2a98dcd942f5ec58bf
Denis Pozo <denis.pozo@dai-labor.de>
2016-08-19 16:17:17 +0200

    add new module for chapter: type information

---
+++ type-information/src/main/java/org/denispozo/tutorial/thj/type/information/App.java
@@ -0,0 +1,13 @@
+package org.denispozo.tutorial.thj.type.information;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
