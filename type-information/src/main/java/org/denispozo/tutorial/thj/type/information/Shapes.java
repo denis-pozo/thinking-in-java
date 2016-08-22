4483750510d0d139c43ef395c8c734dc09a13fc8
Denis Pozo <denis.pozo@dai-labor.de>
2016-08-22 10:49:13 +0200

    add abstract and shape classes to last example

--- type-information/src/main/java/org/denispozo/tutorial/thj/type/information/Shapes.java
+++ type-information/src/main/java/org/denispozo/tutorial/thj/type/information/Shapes.java
@@ -2,12 +2,39 @@ package org.denispozo.tutorial.thj.type.information;
 
 import static org.denispozo.tutorial.thj.util.PrintUtil.print;
 
+import java.util.Arrays;
+import java.util.List;
+
 /*
- * Chapter - Strings
- * Section - Overloading + vs StringBuilder
+ * Chapter - Type Information
+ * Section - The need for Runtime Type Information (RTTI)
  * Example
  */
 
 public class Shapes {
+	public static void main(String[] args) {
+		List<Shape> shapeList = Arrays.asList(new Circle(), new Square(), new Triangle());
+		
+		for(Shape shape : shapeList){
+			shape.draw();
+		}
+	}
+	
+}
 
+abstract class Shape {
+	void draw() { print(this + ".draw()"); }
+	abstract public String toString();
+}
+
+class Circle extends Shape {
+	@Override public String toString() { return "Circle"; } 
+}
+
+class Square extends Shape {
+	@Override public String toString() { return "Square"; } 
+}
+
+class Triangle extends Shape {
+	@Override public String toString() { return "Triangle"; } 
 }
