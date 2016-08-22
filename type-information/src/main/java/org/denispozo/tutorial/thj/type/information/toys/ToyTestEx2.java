159afaa992d819fceb0594a68e1760cdc0251dc3
Denis Pozo <denis.pozo@dai-labor.de>
2016-08-22 12:20:53 +0200

    add exercise 2

---
+++ type-information/src/main/java/org/denispozo/tutorial/thj/type/information/toys/ToyTestEx2.java
@@ -0,0 +1,75 @@
+package org.denispozo.tutorial.thj.type.information.toys;
+
+import static org.denispozo.tutorial.thj.util.PrintUtil.print;
+
+/*
+ * Chapter - Type Information
+ * Section - The Class Object
+ * Example
+ */
+
+public class ToyTestEx2 {
+	
+	@SuppressWarnings("rawtypes")
+	static void printInfo(Class cc) {
+		print("Class name: " + cc.getName() + " is interface? [" +
+			   cc.isInterface() + "]");
+		print("Simple name: " + cc.getSimpleName());
+		print("Canonical name: " + cc.getCanonicalName());
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+		Class c = null;
+		
+		try{
+			c = Class.forName("org.denispozo.tutorial.thj.type.information.toys.FancyToyEx2");
+		} catch (ClassNotFoundException e) {
+			print("Can't find FancyToy");
+			System.exit(1);
+		}
+		printInfo(c);
+		for(Class face : c.getInterfaces()){
+			printInfo(face);
+		}
+		
+		Class up = c.getSuperclass();
+		Object obj = null;
+		try{
+			obj = up.newInstance();
+		} catch (InstantiationException e) {
+			/* Explanation: Exception is thrown here because there is no 
+			 * default constructor for the superclass */
+			print("Can't instantiate"); 
+			System.exit(1);
+		} catch (IllegalAccessException e) {
+			print("Can't access");
+			System.exit(1);
+		}
+		
+		printInfo(obj.getClass());
+		
+	}
+	
+}
+
+
+interface HasBatteriesEx2 {}
+interface WaterproofEx2 {}
+interface ShootsEx2 {}
+interface DancesEx2 {}
+
+class ToyEx2 {
+	/*
+	 * Comment out the following default constructor
+	 * to see NoSuchMethodError from (*1*) 
+	 */
+	ToyEx2() {}
+	ToyEx2(int i) {}
+}
+
+class FancyToyEx2 extends ToyEx2 
+			   implements HasBatteriesEx2, WaterproofEx2, ShootsEx2, DancesEx2{
+	FancyToyEx2() { super(1); }
+
+}
