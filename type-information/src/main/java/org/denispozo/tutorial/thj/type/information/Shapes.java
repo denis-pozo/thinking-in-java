7c7b7f68edc16f74e43d07a4c7793daf0fa4350c
Denis Pozo <denis.pozo@dai-labor.de>
2016-08-22 10:44:15 +0200

    add first example of type information chapter

---
+++ type-information/src/main/java/org/denispozo/tutorial/thj/type/information/Shapes.java
@@ -0,0 +1,13 @@
+package org.denispozo.tutorial.thj.type.information;
+
+import static org.denispozo.tutorial.thj.util.PrintUtil.print;
+
+/*
+ * Chapter - Strings
+ * Section - Overloading + vs StringBuilder
+ * Example
+ */
+
+public class Shapes {
+
+}
