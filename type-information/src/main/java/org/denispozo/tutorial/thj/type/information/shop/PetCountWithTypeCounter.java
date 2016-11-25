3ca0ea42edea67843ee598963e0017c8527cfe17
Denis Pozo <denis.pozo@dai-labor.de>
2016-11-25 16:58:10 +0100

    add counter that uses generic typecounter

---
+++ type-information/src/main/java/org/denispozo/tutorial/thj/type/information/shop/PetCountWithTypeCounter.java
@@ -0,0 +1,22 @@
+package org.denispozo.tutorial.thj.type.information.shop;
+
+import static org.denispozo.tutorial.thj.util.PrintUtil.*;
+
+import org.denispozo.tutorial.thj.type.information.shop.pet.Pet;
+import org.denispozo.tutorial.thj.util.TypeCounter;
+
+public class PetCountWithTypeCounter {
+	
+	public static void main(String[] args) {
+		TypeCounter counter = new TypeCounter(Pet.class);
+		
+		for(Pet pet : Pets.createArray(20)){
+			printnr(pet.getClass().getSimpleName() + " ");
+			counter.count(pet);
+		}
+		print();
+		print(counter);
+	}
+	
+	
+}
