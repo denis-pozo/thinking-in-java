24035ea8762fdfd989dfb244c1fb953625dfedb5
Denis Pozo <denis.pozo@dai-labor.de>
2016-12-06 17:54:25 +0100

    fulfilled the interface robot

--- type-information/src/main/java/org/denispozo/tutorial/thj/type/information/cog/Robot.java
+++ type-information/src/main/java/org/denispozo/tutorial/thj/type/information/cog/Robot.java
@@ -1,5 +1,9 @@
 package org.denispozo.tutorial.thj.type.information.cog;
 
+import java.util.List;
+
+import org.denispozo.tutorial.thj.util.Null;
+
 /*
  * Chapter - Type Information
  * Section - The Null Object
@@ -8,8 +12,21 @@ package org.denispozo.tutorial.thj.type.information.cog;
 
 public interface Robot {
 	
-	String description();
+	String name();
+	String model();
+	List<Operation> opterations();
 	
-	void command();
-
+	class Test {
+		public static void test(Robot r){
+			if(r instanceof Null){
+				System.out.println("[Null Robot]");
+			}
+			System.out.println("Robot name: " + r.name());
+			System.out.println("Robot model: " + r.model());
+			for(Operation operation : r.opterations()){
+				System.out.println(operation.description());
+				operation.command();
+			}
+		}
+	}
 }
