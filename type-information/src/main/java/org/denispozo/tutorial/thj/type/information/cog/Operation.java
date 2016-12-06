99ab36d90794c68efc28c7ae9391a4801a5d7229
Denis Pozo <denis.pozo@dai-labor.de>
2016-12-06 17:48:45 +0100

    add Operation and Robot interfaces

---
+++ type-information/src/main/java/org/denispozo/tutorial/thj/type/information/cog/Operation.java
@@ -0,0 +1,15 @@
+package org.denispozo.tutorial.thj.type.information.cog;
+
+/*
+ * Chapter - Type Information
+ * Section - The Null Object
+ * Example
+ */
+
+public interface Operation {
+	
+	String description();
+	
+	void command();
+
+}
