d67e55941f0a6247eb29a660f585329ae65ce67f
Denis Pozo <denis.pozo@dai-labor.de>
2017-03-30 16:59:22 +0200

    add RandomList and Exercise 6 of generics chapter

---
+++ generics/src/test/java/org/denispozo/tutorial/thj/generics/RandomListEx6.java
@@ -0,0 +1,32 @@
+package org.denispozo.tutorial.thj.generics;
+
+import static org.denispozo.tutorial.thj.util.PrintUtil.printnr;
+
+/*
+ * Chapter - Generics
+ * Section - RandomList
+ * Exercise 6
+ */
+
+public class RandomListEx6<T> {
+	
+	public static void main(String[] args) {
+		RandomList<Integer> rli = new RandomList<Integer>();
+		for(int i = 0 ; i < 10 ; i++ ){
+			rli.add(i);
+		}
+		
+		for(int i = 0 ; i < 15 ; i++) {
+			printnr(rli.select() + " ");
+		}
+		
+		RandomList<Boolean> rlb = new RandomList<Boolean>();
+		for(int i = 0 ; i < 10 ; i++ ){
+			rlb.add(i % 2 == 0);
+		}
+		
+		for(int i = 0 ; i < 15 ; i++) {
+			printnr(rlb.select() + " ");
+		}
+	}
+}
