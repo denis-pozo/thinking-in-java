8036fa52fad062d5ae983949adf4a19d4ec7b613
Denis Pozo <denis.pozo@dai-labor.de>
2017-03-30 14:06:27 +0200

    add Three- and FourTuples inf tuples section

---
+++ generics/src/main/java/org/denispozo/tutorial/thj/generics/tuple/ThreeTuple.java
@@ -0,0 +1,20 @@
+package org.denispozo.tutorial.thj.generics.tuple;
+
+/*
+ * Chapter - Generics
+ * Section - A Tuple Library
+ * Example
+ */
+
+public class ThreeTuple<A, B, C> extends TwoTuple<A, B> {
+	public final C third;
+
+    public ThreeTuple(A a, B b, C c) {
+      super(a,b);
+      third = c;
+    }
+
+    public String toString(){
+    	return "(" + first + ", " + second + ", " + third + ")";
+    }
+}
