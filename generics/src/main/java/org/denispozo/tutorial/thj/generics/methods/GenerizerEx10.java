aaa101c476c6825fcd3315eebffcd3c70143211b
Denis Pozo <denis.pozo@dai-labor.de>
2017-04-11 16:47:55 +0200

    add example and exercise 10

---
+++ generics/src/main/java/org/denispozo/tutorial/thj/generics/methods/GenerizerEx10.java
@@ -0,0 +1,21 @@
+package org.denispozo.tutorial.thj.generics.methods;
+
+/*
+ * Chapter - Generics
+ * Section - Generic Methods
+ * Exercise 10
+ */
+
+public class GenerizerEx10<T> {
+	
+	private T field;
+	
+	public void setField(T t){
+		field = t;
+	}
+	
+	public T getField(){
+		return field;
+	}
+
+}
---
+++ generics/src/main/java/org/denispozo/tutorial/thj/generics/methods/New.java
@@ -0,0 +1,55 @@
+package org.denispozo.tutorial.thj.generics.methods;
+
+import static org.denispozo.tutorial.thj.util.PrintUtil.print;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+/*
+ * Chapter - Generics
+ * Section - Leveraging type argument inference
+ * Example
+ */
+
+public class New {
+	
+	public static <K,V> Map<K,V> map() {
+		return new HashMap<K,V>();
+	}
+	
+	public static <T> List<T> list() {
+		return new ArrayList<T>();
+	}
+	
+	public static <T> LinkedList<T> lList() {
+		return new LinkedList<T>();
+	}
+	
+	public static <T> Set<T> set() {
+		return new HashSet<T>();
+	}
+	
+	public static <T> Queue<T> queue() {
+		return new LinkedList<T>();		
+	}
+	
+	public static <T> GenerizerEx10<T> generizer() {
+		return new GenerizerEx10<T>();
+	}
+	
+	public static void main(String[] args) {
+		Map<String, List<String>> ls = New.map();
+		print(ls.getClass());
+		GenerizerEx10<String> gen = New.generizer();
+		gen.setField("Car");
+		print(gen.getField());
+	}
+	
+	
+}
