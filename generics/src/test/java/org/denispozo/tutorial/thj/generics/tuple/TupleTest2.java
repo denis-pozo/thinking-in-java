19dbc66268279e769240a53d6868b949247cafae
Denis Pozo <denis.pozo@dai-labor.de>
2017-05-08 14:51:56 +0200

    add example of simplifying tuples

--- generics/src/test/java/org/denispozo/tutorial/thj/generics/tuple/TupleTest2.java
+++ generics/src/test/java/org/denispozo/tutorial/thj/generics/tuple/TupleTest2.java
@@ -1,36 +1,39 @@
 package org.denispozo.tutorial.thj.generics.tuple;
 
+import static org.denispozo.tutorial.thj.generics.tuple.Tuple.*;
+
 /*
  * Chapter - Generics
- * Section - A Tuple Library
- * Example and part of Ex3
+ * Section - Simplifying tuple use
+ * Example
  */
 
-public class TupleTest {
+public class TupleTest2 {
 	
 	static TwoTuple<String, Integer> f(){
-		return new TwoTuple<String, Integer>("hi", 47);
+		return tuple("hi", 47);
 	}
 	
 	static ThreeTuple<Amphibian, String, Integer> g(){
-		return new ThreeTuple<Amphibian, String, Integer>(new Amphibian(), "hi", 47);
+		return tuple(new Amphibian(), "hi", 47);
 	}
 	
 	static FourTuple<Vehicle, Amphibian, String, Integer> h(){
-		return new FourTuple<Vehicle, Amphibian, String, Integer>(new Vehicle(), new Amphibian(), "hi", 47);
+		return tuple(new Vehicle(), new Amphibian(), "hi", 47);
 	}
 	
 	static FiveTuple<Vehicle, Amphibian, String, Integer, Double> k(){
-		return new FiveTuple<Vehicle, Amphibian, String, Integer, Double>(new Vehicle(), new Amphibian(), "hi", 47, 11.1);
+		return tuple(new Vehicle(), new Amphibian(), "hi", 47, 11.1);
 	}
 	
 	static SixTupleEx3<Vehicle, Amphibian, String, Integer, Double, Boolean> l(){
-		return new SixTupleEx3<Vehicle, Amphibian, String, Integer, Double, Boolean>(new Vehicle(), new Amphibian(), "hi", 47, 11.1, true);
+		return tuple(new Vehicle(), new Amphibian(), "hi", 47, 11.1, true);
 	}
 	
 	public static void main(String[] args) {
 		TwoTuple<String, Integer> ttsi = f();
 		System.out.println(ttsi);
+//		ttsi.first = "there";
 		System.out.println(g());
 		System.out.println(h());
 		System.out.println(k());
@@ -38,5 +41,5 @@ public class TupleTest {
 	}
 }
 
-class Amphibian {}
-class Vehicle {}
+class Amphibian2 {}
+class Vehicle2 {}
