cd7f6e6cc1ee3d5706f5c4c4608b58a9bd89375c
Denis Pozo <denis.pozo@dai-labor.de>
2017-06-01 16:42:22 +0200

    add examples from section generic arrays

---
+++ generics/src/main/java/org/denispozo/tutorial/thj/generic/arrays/ArrayOfGeneric.java
@@ -0,0 +1,25 @@
+package org.denispozo.tutorial.thj.generic.arrays;
+
+/*
+ * Chapter - Generics
+ * Section - Arrays of Generics
+ * Example
+ */
+
+public class ArrayOfGeneric {
+
+	static final int SIZE = 100;
+	static Generic<Integer> [] gia;
+
+	public static void main(String[] args) {
+		// Compiles; produces a ClassCastExeption
+//		gia = (Generic<Integer>[]) new Object[SIZE];
+		// Runtime type is the raw (erased) type:
+		gia = (Generic<Integer>[]) new Generic[SIZE];
+		System.out.println(gia.getClass().getSimpleName());
+		gia[0] = new Generic<Integer>();
+//		gia[1] = new Object();
+//		gia[2] = new Generic<Double>();
+	}
+}
+
---
+++ generics/src/main/java/org/denispozo/tutorial/thj/generic/arrays/ArrayOfGenericReference.java
@@ -0,0 +1,14 @@
+package org.denispozo.tutorial.thj.generic.arrays;
+
+/*
+ * Chapter - Generics
+ * Section - Arrays of Generics
+ * Example
+ */
+
+public class ArrayOfGenericReference {
+	static Generic<Integer> [] gia;
+	
+}
+
+class Generic<T> {}
