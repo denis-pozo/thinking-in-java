2daf1fdd176ba454c50a2ef91d4b8ee89bb6f11d
Denis Pozo <denis.pozo@dai-labor.de>
2017-07-05 15:28:48 +0200

    add exercise 27 and examples

---
+++ generics/src/main/java/org/denispozo/tutorial/thj/generic/wildcards/CompilerIntelligence.java
@@ -0,0 +1,21 @@
+package org.denispozo.tutorial.thj.generic.wildcards;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Chapter - Generics
+ * Section - Wildcards
+ * Example
+ */
+
+public class CompilerIntelligence {
+    public static void main(String[] args) {
+        List<? extends Fruit> flist = Arrays.asList(new Apple());
+        Apple a = (Apple)flist.get(0);
+        flist.contains(new Apple());
+        flist.indexOf(new Apple());
+    }
+}
+
+
