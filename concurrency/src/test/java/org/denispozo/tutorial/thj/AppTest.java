1afc6efec3d5dc009f8d91a953d3bf47a962e4e2
Denis Pozo <denis.pozo@dai-labor.de>
2019-05-04 12:06:34 +0200

    add concurrency module

---
+++ concurrency/src/test/java/org/denispozo/tutorial/thj/AppTest.java
@@ -0,0 +1,20 @@
+package org.denispozo.tutorial.thj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
